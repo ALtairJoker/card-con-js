c294b2c3869e1e09326b506e9c21de1418ad91cf
Alexis <ajeria@outlook.es>
2022-09-08 00:10:19 -0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+function compraTotal () {
+
+    // Seleccionando variables de cantidad y precio
+
+    let precio = document.querySelector('#precio');
+    let cantidad = document.querySelector('#cantidad');
+    let cantidadFinal = document.querySelector('#cantidadFinal');
+    let total = document.querySelector('#total');
+
+    // operacion para el total y cantidad
+
+    resultado = Number(precio.textContent) * Number(cantidad.value);
+    total.innerHTML = resultado;
+    cantidadFinal.innerHTML = cantidad.value
+
+    // seleccionando variable color y agregandolo
+    let color = document.querySelector('#color');
+    let card = document.querySelector('#card');
+    let circulo = document.querySelector('#circulo');
+    card.style.backgroundColor = color.value;
+    circulo.style.backgroundColor = color.value;
+
+}
